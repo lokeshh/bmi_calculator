6aa374e0fa9e11230512a043402f063f28b7eeee
Lokesh Sharma <lokeshhsharma@gmail.com>
2020-02-05 15:21:36 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,63 +6,65 @@ from adafruit_ads1x15.analog_in import AnalogIn
 import time
 import sys
 import RPi.GPIO as GPIO
-GPIO.setwarnings(False)
 
-EMULATE_HX711=False
-referenceUnit = 1
+def main():
+    GPIO.setwarnings(False)
 
-if not EMULATE_HX711:
-    import RPi.GPIO as GPIO
-    from hx711 import HX711
-else:
-    from emulated_hx711 import HX711
-
-def cleanAndExit():
-    print("Cleaning...")
+    EMULATE_HX711=False
+    referenceUnit = 1
 
     if not EMULATE_HX711:
-        GPIO.cleanup()
-        
-    print("Bye!")
-    sys.exit()
+        import RPi.GPIO as GPIO
+        from hx711 import HX711
+    else:
+        from emulated_hx711 import HX711
 
-hx = HX711(5, 6)
-hx.set_reading_format("MSB", "MSB")
-hx.set_reference_unit(referenceUnit)
-hx.reset()
-hx.tare()    
-# Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
-# Create the ADC object using the I2C bus
-ads = ADS.ADS1015(i2c)
-# Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
-# Create differential input between channel 0 and 1
-#chan = AnalogIn(ads, ADS.P0, ADS.P1)
-#print("{:>5}\t{:>5}".format('raw', 'v'))
-while True:
-    height = ((chan.value-(chan.value*0.9))/18)+8
-    val = hx.get_weight(5)
-    weight=((val*0.466)/10000)
-    hx.power_down()
-    hx.power_up()
-    time.sleep(0.1)
-    print("{:>5}/t{}".format(height, weight))
-    time.sleep(0.5)
-                                   
+    def cleanAndExit():
+        print("Cleaning...")
 
-#print("Tare done! Add weight now...")
+        if not EMULATE_HX711:
+            GPIO.cleanup()
 
-# to use both channels, you'll need to tare them both
-#hx.tare_A()
-#hx.tare_B()
+        print("Bye!")
+        sys.exit()
 
-#while True:
-    try:
-        # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-       
+    hx = HX711(5, 6)
+    hx.set_reading_format("MSB", "MSB")
+    hx.set_reference_unit(referenceUnit)
+    hx.reset()
+    hx.tare()    
+    # Create the I2C bus
+    i2c = busio.I2C(board.SCL, board.SDA)
+    # Create the ADC object using the I2C bus
+    ads = ADS.ADS1015(i2c)
+    # Create single-ended input on channel 0
+    chan = AnalogIn(ads, ADS.P0)
+    # Create differential input between channel 0 and 1
+    #chan = AnalogIn(ads, ADS.P0, ADS.P1)
+    #print("{:>5}\t{:>5}".format('raw', 'v'))
+    while True:
+        height = ((chan.value-(chan.value*0.9))/18)+8
+        val = hx.get_weight(5)
+        weight=((val*0.466)/10000)
+        hx.power_down()
+        hx.power_up()
+        time.sleep(0.1)
+        print("{:>5}/t{}".format(height, weight))
+        time.sleep(0.5)
 
-    except (KeyboardInterrupt, SystemExit):
-        cleanAndExit()
+
+    #print("Tare done! Add weight now...")
+
+    # to use both channels, you'll need to tare them both
+    #hx.tare_A()
+    #hx.tare_B()
+
+    #while True:
+        try:
+            # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
+
+
+        except (KeyboardInterrupt, SystemExit):
+            cleanAndExit()
 
 
